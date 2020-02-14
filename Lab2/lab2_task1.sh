77f32229f7ef2ba929b3fd2b1c08c1c4059323d5
Emmanuel <aadewale123@gmail.com>
2020-02-14 15:58:06 -0500

    Part 2

--- Lab2/lab2_task1.sh
+++ Lab2/lab2_task1.sh
@@ -4,18 +4,29 @@ num_writer() {
 	if [ "$#" -lt 1 ]; then
 		usage
 	elif [ "$#" -eq 1 ]; then
-		echo "We have one input"
 		shuf -i 1-32767 -n $1 > rands_$1.txt
 		small=$(cat rands_$1.txt | sort -n | head -n 1)
 		large=$(cat rands_$1.txt | sort -n | tail -n 1)
 		echo "You requested $1 numbers"
 		echo "The smallest value generated was $small"
 		echo "The largest value generated was $large"
+		average=0;
+		while read line; do
+			average=$(($average + $line))
+		done < rands_$1.txt
 		echo "The average value generated was $average"
 	elif [ "$#" -eq 3 ]; then
-		echo "THeree"
-	else
-		echo "Too much arguments. Bye!!!"
+		shuf -i $2-$3 -n $1 > rands_$1.txt
+		small=$(cat rands_$1.txt | sort -n | head -n 1)
+		large=$(cat rands_$1.txt | sort -n | tail -n 1)
+		echo "You requested $1 numbers"
+		echo "The smallest value generated was $small"
+		echo "The largest value generated was $large"
+		average=0;
+		while read line; do
+			average=$(($average + $line))
+		done < rands_$1.txt
+		echo "The average value generated was $average"
 	fi
 }	
 
@@ -29,11 +40,3 @@ min=$2
 max=$3
 
 num_writer $num_rands $min $max
-
-#shuf -i 0-50 -n $num_rands
-#for ((i=1; i <= $num_rands; i++))
-#do
-#	echo $i
-#	shuf -i 0-50 -n $i
-#done
-
