1b03e931da660132a7323c77fa79797b3e7e0970
Emmanuel <aea8506@rit.edu>
2020-03-21 13:13:56 -0400

    Trying again

--- Lab3/lab3_task2.py
+++ Lab3/lab3_task2.py
@@ -10,6 +10,7 @@ def main():
     bin_dir = os.listdir("/usr/bin/")
     #print(bin_dir)
     temp=[]
+    md5list = []
     for i in bin_dir:
        elem_list = []
        elem_list.append(i)
@@ -19,9 +20,21 @@ def main():
     with open(filename) as f:
 	for l in f:
 	    line = l.strip().split(" ")
-	    print(line)
+	   # print(line)
+	    md5list.append(line)
 
-
+    #print("temp")
+    #print(temp)
+    #print("md5")
+    #print(md5list)
+    for ele in temp:
+	for md5 in md5list:
+	    #print("element:", ele[1])
+	    #print(md5[1])
+	    if ele[0] == md5[0]:
+		if ele[1] != md5[1]:
+		    print(str(ele[0]) + ": MD5 original = " + ele[1] + ", new MD5 = " + md5[1])
+		    break
 
 if __name__ == "__main__":
     main()
