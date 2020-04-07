727c7bf94d090deb0c989ddc1af78c4c91f94f34
emmaunel <aea8506@rit.edu>
2020-04-07 12:03:08 -0400

    stuff

--- Lab3/lab3_task2.py
+++ Lab3/lab3_task2.py
@@ -2,35 +2,49 @@ import sys
 import os
 import hashlib
 
+md5orig = "md5_orig.txt"
 def main():
     if len(sys.argv) < 2:
 		print("Usage: python lab3_task2.py <filename>")
 		exit(0)
     filename = sys.argv[1]
-    bin_dir = os.listdir("/usr/bin/")
-    #print(bin_dir)
-    temp=[]
-    md5list = []
-    for i in bin_dir:
-       elem_list = []
-       elem_list.append(i)
-       elem_list.append(hashlib.md5(i).hexdigest())
-       temp.append(elem_list)
-   # print(temp)
-    with open(filename) as f:
-	for l in f:
-	    line = l.strip().split(" ")
-	   # print(line)
-	    md5list.append(line)
+    
+    md5origlist = []
+    with open(md5orig) as f:
+		for l in f:
+			line = l.strip().split(" ")
+			md5origlist.append(line)
 
+    #print(str(md5origlist))
+    md5new = []
+    with open(filename) as f:
+		for l in f:
+			line = l.strip().split(" ")
+			md5new.append(line)
+
+    for ele in md5origlist:
+    	#print(ele[0])
+        for md5 in md5new:
+			#print(md5[0])
+			if ele[0] == md5[0]:
+				#print("Match: " + md5[1])
+				if ele[1] == md5[1]:
+					continue
+				else:
+					print(str(ele[0]) + ": MD5 original = " + md5[1] + ", new MD5 = " + ele[1])
+				break
+
+				#if ele[1] != md5[1]:
+			#	print(str(ele[0]) + ": MD5 original = " + md5[1] + ", new MD5 = " + ele[1])
+				#print(ele)
     # Comparing both list
     # temp contains md5 from the file(md5.txt)
     # md5list contains md5 from the script(system)
-    for ele in temp:
-	for md5 in md5list:
-	    if ele[0] == md5[0]:
+    #for ele in md5new:
+	#for md5 in md5orig:
+	 #   if ele[0] == md5[0]:
 		#if ele[1] != md5[1]:
-		print(str(ele[0]) + ": MD5 original = " + md5[1] + ", new MD5 = " + ele[1])
+		#print(str(ele[0]) + ": MD5 original = " + md5[1] + ", new MD5 = " + ele[1])
 
 if __name__ == "__main__":
     main()
